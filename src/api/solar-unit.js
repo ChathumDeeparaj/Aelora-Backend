ad6d8628171f8bc160a99879599ccaaa32efbda9
chathum <deeparajchathum@gmail.com>
2025-11-08 03:10:57 +0530

    Initial commit on dev branch

--- src/api/solar-unit.js
+++ src/api/solar-unit.js
@@ -1,5 +1,14 @@
 import express from "express";
+import { 
+  getAllSolarUnits, 
+  createSolarUnit, 
+  getSolarUnitById 
+} from "../application/solar-unit.js";
 
 const solarUnitRouter = express.Router();
+
+solarUnitRouter.route("/").get(getAllSolarUnits).post(createSolarUnit);
+solarUnitRouter.route("/:id").get(getSolarUnitById);
+// solarUnitRouter.route("/:id").get().put().delete();
 
 export default solarUnitRouter;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import express from "express";
 import solarUnitRouter from "./api/solar-unit.js";
 
-
 const server = express();
+server.use(express.json());
+
 
 server.use("/api/solar-units", solarUnitRouter);
-
-server.get("/api", (req, res) => {
-    res.status(200).json({ message: "Hello World" });
-});
 
 const PORT = 8000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
